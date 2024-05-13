ca73e981822ccf6c84eb0c5ee6348e045677a33d
kbardach <144119408+kbardach@users.noreply.github.com>
2024-05-13 10:49:02 +0200

    string changed

--- VerificationProvider/Program.cs
+++ VerificationProvider/Program.cs
@@ -10,11 +10,11 @@ using VerificationProvider.Services;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWebApplication()
-    .ConfigureServices((context, services ) =>
+    .ConfigureServices((context, services) =>
     {
         services.AddApplicationInsightsTelemetryWorkerService();
         services.ConfigureFunctionsApplicationInsights();
-        services.AddDbContext<DataContext>(x => x.UseSqlServer(context.Configuration.GetConnectionString("VerificationRequestDatabase")));
+        services.AddDbContext<DataContext>(x => x.UseSqlServer(Environment.GetEnvironmentVariable("VerificationRequestDatabase")));
         services.AddScoped<IVerificationService, VerificationService>();
         services.AddScoped<IVerificationCleanerService, VerificationCleanerService>();
         services.AddScoped<IValidateVerificationCodeService, ValidateVerificationCodeService>();
@@ -43,4 +43,52 @@ var host = new HostBuilder()
 host.Run();
 
 
-//Debug.WriteLine($"ERROR : VerificationProvider.Program.cs :: {ex.Message}");
+
+
+
+//-----------------------------------------------------------------------------------------------------------------------------------------
+
+//using Microsoft.Azure.Functions.Worker;
+//using Microsoft.EntityFrameworkCore;
+//using Microsoft.Extensions.DependencyInjection;
+//using Microsoft.Extensions.Hosting;
+//using System.Diagnostics;
+//using VerificationProvider.Data.Contexts;
+//using VerificationProvider.Services;
+
+
+//var host = new HostBuilder()
+//    .ConfigureFunctionsWebApplication()
+//    .ConfigureServices(services =>
+//    {
+//        services.AddApplicationInsightsTelemetryWorkerService();
+//        services.ConfigureFunctionsApplicationInsights();
+//        services.AddDbContext<DataContext>(x => x.UseSqlServer(Environment.GetEnvironmentVariable("VerificationRequestDatabase")));
+//        services.AddScoped<IVerificationService, VerificationService>();
+//        services.AddScoped<IVerificationCleanerService, VerificationCleanerService>();
+//        services.AddScoped<IValidateVerificationCodeService, ValidateVerificationCodeService>();
+
+//    })
+//    .Build();
+
+//using (var scope = host.Services.CreateScope())
+//{
+//    try
+//    {
+//        var context = scope.ServiceProvider.GetRequiredService<DataContext>();
+//        var migration = context.Database.GetPendingMigrations();
+//        if (migration != null && migration.Any())
+//        {
+//            context.Database.Migrate();
+//        }
+//    }
+//    catch (Exception ex)
+//    {
+//        Debug.WriteLine($"ERROR : VerificationProvider.Program.cs :: {ex.Message}");
+//    }
+
+//}
+
+//host.Run();
+
+
